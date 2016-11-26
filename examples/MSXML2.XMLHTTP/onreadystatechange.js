31daba697655dd10f13343cab1b191c183811c2c
mrpapercut <mischa.rodermond@gmail.com>
2016-11-26 12:05:20 +0100

    Adding full onreadystatechange example

--- examples/MSXML2.XMLHTTP/onreadystatechange.js
+++ examples/MSXML2.XMLHTTP/onreadystatechange.js
@@ -1,3 +1,55 @@
-var req = WScript.CreateObject('MSXML2.XMLHTTP');
-req.readyState = 1;
-// req.open('GET', 'http://www.x0f.re', false);
+var stream = WScript.CreateObject('MSXML2.XMLHTTP');
+stream.open('GET', 'http://x0f.re/loggetrequests', false);
+
+// 0 UNINTIALIZED
+// 1 LOADING
+// 2 LOADED
+// 3 INTERACTIVE
+// 4 COMPLETED
+
+var ts = ots = null;
+
+var states = {
+    0: {
+        readyState: 'UNINITIALIZED',
+        time: 0
+    },
+    1: {
+        readyState: 'LOADING',
+        time: null
+    },
+    2: {
+        readyState: 'LOADED',
+        time: null
+    },
+    3: {
+        readyState: 'INTERACTIVE',
+        time: null
+    },
+    4: {
+        readyState: 'COMPLETED',
+        time: null
+    }
+}
+
+stream.onreadystatechange = function() {
+    var msg = '';
+
+    switch(stream.readyState) {
+        default:
+            var newts = +new Date;
+            states[stream.readyState].time = newts - ts;
+            ts = newts;
+            break;
+    }
+
+    if (stream.readyState === 4) {
+        for (var i in states) {
+            WScript.Echo([states[i].readyState, states[i].time + 'ms'].join('\r\n'));
+        }
+        WScript.Echo('Total time: ' + (ts - ots) + 'ms');
+    }
+}
+
+ts = ots = +new Date;
+stream.send();
