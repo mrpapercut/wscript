020a1a9847d846a456098c97a5ed3253fc4acafb
mrpapercut <mischa.rodermond@gmail.com>
2017-05-21 13:12:24 -0400

    examples

--- examples/Scripting.FileSystemObject/FSO/FileActions.js
+++ examples/Scripting.FileSystemObject/FSO/FileActions.js
@@ -1,3 +1,16 @@
 var fso = new ActiveXObject('Scripting.FileSystemObject');
 
-WScript.Echo(fso.GetDrive('C'));
+var f = fso.CreateTextFile('test.txt');
+f.WriteLine('Hello world!');
+var getStats = function() {
+    WScript.Echo(f.Line);
+    WScript.Echo(f.Column);
+    WScript.Echo(f.AtEndOfStream);
+    WScript.Echo(f.AtEndOfLine);
+};
+
+// WScript.Echo(ts.Skip(4));
+getStats();
+// WScript.Echo(ts.ReadAll());
+// WScript.Echo(ts.Read(8));
+//getStats();
