dfbe192a822ba4accb63ac2419f32d1fca8aa263
mrpapercut <mischa.rodermond@gmail.com>
2017-05-21 12:44:36 -0400

    Replace sloppy workarounds for TextStream with actual functionality

--- test/common/TextStream.spec.js
+++ test/common/TextStream.spec.js
@@ -8,9 +8,9 @@ var getFilePath = function(filename) {
     return path.join(__dirname, '../..', 'lib', filename);
 }
 
-var getNewInstance = function() {
+var getNewInstance = function(filename, contents, unicode, iomode) {
     var instance = require(getFilePath('common/TextStream'));
-    return new instance();
+    return new instance(filename, contents, unicode, iomode);
 }
 
 var getResponseText = function() {
@@ -22,6 +22,37 @@ var TextStream;
 describe('TextStream', function() {
     beforeEach(function() {
         TextStream = getNewInstance();
+    });
+
+    describe('constructor()', function() {
+        var properties = {
+            AtEndOfLine:   true,
+            AtEndOfStream: true,
+            Column:        1,
+            Line:          1,
+            _filename:     'testfile.txt',
+            _unicode:      false,
+            _iomode:       1,
+            _contents:     ''
+        };
+
+        it('should have all properties', function() {
+            TextStream = getNewInstance('testfile.txt', '', false);
+            expect(TextStream).to.have.all.keys(Object.keys(properties));
+        });
+
+        it('should have all default values', function() {
+            TextStream = getNewInstance('testfile.txt', '', false);
+            for (var i in properties) {
+                expect(TextStream[i]).to.eql(properties[i]);
+            }
+        });
+
+        it('should throw error if initialized with invalid iomode', function() {
+            expect(function() {
+                getNewInstance('testfile.txt', '', false, -1);
+            }).to.throw(TypeError);
+        });
     });
 
     describe('Close()', function() {
@@ -38,14 +69,14 @@ describe('TextStream', function() {
         });
 
         it('should return a string of 10 characters', function() {
-            TextStream.Write('abcdefghijklmnopqrstuvwxyz');
+            TextStream = getNewInstance('testfile.txt', 'abcdefghijklmnopqrstuvwxyz', true, 1);
+
             expect(TextStream.Read(10)).to.have.lengthOf(10);
         });
 
         var ResponseText = getResponseText();
         it('should return all text if argument is larger than ResponseText.length', function() {
-            TextStream = getNewInstance();
-            TextStream.Write(ResponseText);
+            TextStream = getNewInstance('testfile.txt', ResponseText, true, 1);
 
             expect(TextStream.Read(ResponseText.length + 10)).to.have.lengthOf(ResponseText.length);
         });
@@ -55,9 +86,16 @@ describe('TextStream', function() {
         var ResponseText = getResponseText();
 
         it('should return all text', function() {
-            TextStream.Write(ResponseText);
+            TextStream = getNewInstance('testfile.txt', ResponseText, true, 1);
 
             expect(TextStream.ReadAll()).to.have.lengthOf(ResponseText.length);
+        });
+
+        it('should set AtEndOfLine and AtEndOfStream to true', function() {
+            TextStream.Write(ResponseText);
+            TextStream.ReadAll();
+            expect(TextStream.AtEndOfLine).to.be.true;
+            expect(TextStream.AtEndOfStream).to.be.true;
         });
     });
 
@@ -65,7 +103,7 @@ describe('TextStream', function() {
         var ResponseText = getResponseText();
 
         it('should return the first line of ResponseText', function() {
-            TextStream.Write(ResponseText);
+            TextStream = getNewInstance('testfile.txt', ResponseText, true, 1);
 
             expect(TextStream.ReadLine()).to.eql('The MIT License (MIT)');
         });
@@ -75,9 +113,10 @@ describe('TextStream', function() {
         var ResponseText = getResponseText();
 
         it('should skip 6 characters and return the rest', function() {
-            TextStream.Write(ResponseText);
-
-            expect(TextStream.Skip(6)).to.have.lengthOf(ResponseText.length - 6);
+            TextStream = getNewInstance('testfile.txt', ResponseText, true, 1);
+            TextStream.Skip(25);
+            expect(TextStream.Column).to.equal(3);
+            expect(TextStream.Line).to.equal(3);
         });
     });
 
@@ -97,6 +136,13 @@ describe('TextStream', function() {
 
             expect(TextStream._contents).to.eql('Hello world!');
         });
+
+        it('should throw error when writing unicode without specifying', function() {
+            TextStream = getNewInstance('testfile.txt', '', false, 2);
+            expect(function() {
+                TextStream.Write('ĀāĂ');
+            }).to.throw(TypeError);
+        });
     });
 
     describe('WriteBlankLines()', function() {
@@ -108,12 +154,21 @@ describe('TextStream', function() {
 
     describe('WriteLine()', function() {
         it('should write line to TextStream._contents', function() {
+            TextStream = getNewInstance('testfile.txt', '', false, 8);
             TextStream.WriteLine('Hello World!');
             expect(TextStream._contents).to.eql('Hello World!\n');
         });
+
         it('should write blank line if no string provided', function() {
             TextStream.WriteLine();
             expect(TextStream._contents).to.eql('\n');
+        });
+
+        it('should throw error when writing unicode without specifying', function() {
+            TextStream = getNewInstance('testfile.txt', '', false, 2);
+            expect(function() {
+                TextStream.WriteLine('ĀāĂ');
+            }).to.throw(TypeError);
         });
     });
 });
