653d08ccd5b4389232534d75d3ed2810aeadef82
mrpapercut <mischa.rodermond@gmail.com>
2017-05-26 17:19:02 +0200

    Removing console.log

--- examples/WScript/ActiveXObject.js
+++ examples/WScript/ActiveXObject.js
@@ -1,1 +1,6 @@
-var axo = WScript.CreateObject('ActiveXObject');
+//var app = new ActiveXObject('Shell.Application');
+var app = new ActiveXObject('System.Random');
+WScript.Echo(app.GetHashCode());
+for (var i in app) {
+    WScript.Echo(i, app[i]);
+}
