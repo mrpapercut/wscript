dd0cc792ad11cd9481bddcdf88d4780d199cf7e2
mrpapercut <mischa.rodermond@gmail.com>
2017-06-25 16:01:16 +0200

    Testing

--- examples/ADODB.Stream/Write.js
+++ examples/ADODB.Stream/Write.js
@@ -1,8 +1,8 @@
 var ADODBStream = WScript.CreateObject('ADODB.Stream');
 ADODBStream.open();
 ADODBStream.type = 1;
-// ADODBStream.loadFromFile('examples//ADODB.Stream//Write.js');
-ADODBStream.write(01011001);
+ADODBStream.loadFromFile('examples//ADODB.Stream//empty.js');
+// ADODBStream.write(01011001);
 ADODBStream.position = 0;
 var bA = ADODBStream.read();
 
@@ -10,5 +10,7 @@ var stream = WScript.CreateObject('ADODB.Stream');
 stream.open();
 stream.type = 1;
 stream.write(bA);
+WScript.Echo(stream.position);
 stream.position = 0;
 WScript.Echo(stream.read(2));
+WScript.Echo(stream.size);
--- examples/ADODB.Stream/WriteText.js
+++ examples/ADODB.Stream/WriteText.js
@@ -1,10 +1,15 @@
 var stream1 = WScript.CreateObject('ADODB.Stream'),
 	stream2 = WScript.CreateObject('ADODB.Stream');
 stream1.open();
-stream1.writeText('Hello world', 1);
+stream1.writeText('Hello world');
 WScript.Echo(stream1.size, stream1.position);
 stream1.position = 0;
 stream1.writeText('Goodbye', 1);
 WScript.Echo(stream1.size, stream1.position);
 stream1.position = 0;
 WScript.Echo(stream1.readText());
+stream1.position = 0;
+stream2.open();
+stream1.copyTo(stream2, 4);
+stream2.position = 0;
+WScript.Echo(stream2.readText());
